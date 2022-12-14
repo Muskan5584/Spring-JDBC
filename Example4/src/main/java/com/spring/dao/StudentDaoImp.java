41ce1dcb60d128b70d02b8017fd80a83c9280fc5
Muskan Yadav <muskanyadav5584@gmail.com>
2022-12-15 01:19:27 +0530

    Fetching single row from database

---
+++ Example4/src/main/java/com/spring/dao/StudentDaoImp.java
@@ -0,0 +1,40 @@
+package com.spring.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import com.spring.model.StudentModel;
+
+  /*
+   * Repository annotation is indicates that, these class is dealing with Database CRUD operation.
+   * Spring container is responsible to create the instance of these @Repository annotated class. 
+   */
+@Repository("studentDao")
+public class StudentDaoImp implements StudentDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public StudentModel getStudentData(StudentModel studentModel) {
+        String query="select * from HSStudents where id=?";
+
+        //creating object for RowMapper Interface.
+        RowMapper<StudentModel> rowMapper=new RowMapper<StudentModel>() {
+			public StudentModel mapRow(ResultSet rs, int rowNum) throws SQLException {
+				StudentModel studentModel=new StudentModel();
+				studentModel.setId(rs.getInt(1));
+				studentModel.setName(rs.getString(2));
+				studentModel.setContact(rs.getInt(3));
+				studentModel.setTotalMarks(rs.getInt(4));
+				studentModel.setObtainMarks(rs.getInt(5));
+				return studentModel;
+			}
+        };
+        return this.jdbcTemplate.queryForObject(query, rowMapper, studentModel.getId());
+	}
+
+}
